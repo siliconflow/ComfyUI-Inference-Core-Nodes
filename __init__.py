2a5ee946307fb80a2c509bd8e42e1f38f03ad814
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2025-05-19 23:52:08 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from inference_core_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .src.inference_core_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ("NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS")
